a69f9d55221cf073a5e5c6a3d3ac505ac4685775
vikram singh <omvikramsingh07@gmail.com>
2026-05-27 21:55:13 +0530

    login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Banner from '../components/home/Banner'
 import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
+import Testimonial from '../components/home/Testimonial'
+import CallToAction from '../components/home/CallToAction'
+import Footer from '../components/home/Footer'
+
 
 const Home = () => {
   return (
@@ -9,7 +13,10 @@ const Home = () => {
         <Banner />
         <Hero />
         <Features />
-
+        <Testimonial />
+        <CallToAction />
+        <Footer />
+        
     </div>
   )
 }
