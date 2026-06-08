227fad650657c5fb5d2b3f50a7d0699336bdb1ec
vikram singh <omvikramsingh07@gmail.com>
2026-06-08 22:45:57 +0530

    route

---
+++ Server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { getUserById, loginUser, registerUser } from '../controllers/userController.js';
+import protect from '../middlewares/authMiddleware.js';
+
+
+
+const userRouter = express.Router();
+
+userRouter.post('/register', registerUser);
+userRouter.post('/register', loginUser);
+userRouter.get('/data', protect, getUserById);
+
+
+export default userRouter;
