19f799850f692cdf59db6987aca5a3a5bcaebf31
vikram singh <omvikramsingh07@gmail.com>
2026-06-08 21:07:34 +0530

    Added frontend and backend

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (        
+        <div className="flex flex-wrap items-center justify-between w-full px-4 md:px-14 py-2 font-medium text-sm text-white text-center bg-gradient-to-r from-violet-500 to-purple-100">
+            <p><span className="px-3 py-1 rounded-lg text-white bg-purple-700 mr-2">New</span>AI Feature Added</p>
+        </div>
+  )
+}
+
+export default Banner
