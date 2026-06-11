728e5678c772d65c8358bb20830be01043a83c9f
vikram singh <omvikramsingh07@gmail.com>
2026-06-11 11:03:08 +0530

    img

--- Server/controllers/resumeController.js
+++ Server/controllers/resumeController.js
@@ -1,4 +1,6 @@
 import Resume from " .. /models/Resume.js";
+import imagekit from "../configs/imageKit.js";
+import fs from 'fs';
 
 
 
@@ -87,6 +89,22 @@ export const updateResume = async (req, res) =>{
     const image = req.file;
 
     let resumeDataCopy = JSON.parse(resumeData);
+
+    if(image){
+
+        const imageBufferData = fs.createReadStream(image.path)
+
+        const response = await imagekit.files.upload({
+            file: imageBufferData,
+            fileName: 'resume.png',
+            folder: 'user-resumes',
+            transformation: {
+                pre: 'w-300, h-300, fo-face, z-0.75' + (removeBackground ? ',e-bgremove' : '')
+            }
+        });
+        resumeDataCopy.personal_info.image = response.url
+
+    }
 
     const resume = await Resume.findByIdAndUpdate({userId, _id: resumeId},
     resumeDataCopy, {new: true})
