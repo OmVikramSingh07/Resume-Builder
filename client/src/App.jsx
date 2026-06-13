284c30e5df9a2bbbd8a8262fce28b99281a1979e
vikram singh <omvikramsingh07@gmail.com>
2026-06-13 21:25:11 +0530

    start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Layout from './pages/Layout'
@@ -6,8 +6,35 @@ import Dashboard from './pages/Dashboard'
 import ResumeBuilder from './pages/ResumeBuilder'
 import Preview from './pages/Preview'
 import Login from './pages/Login'
+import { useDispatch } from 'react-redux'
+import { login, setLoading } from './app/features/authslice'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  const getUserData = async () => {
+    const token = localStorage.getItem('token')
+    try {
+      if(token){
+        const {data} = await api.get('/api/user/data', {headers: {Authorization: token}})
+        if(data.user){
+          dispatch(login({toekn, user: data.user}))
+        }
+        dispatch(setLoading(false))
+      }else{
+        dispatch(setLoading(false))
+      }
+    } catch (error) {
+      dispatch(setLoading(false))
+      console.log(error.message)
+    }
+    
+  }
+
+  useEffect(()=> {
+    getUserData()
+  }, [])
   return (
     <>
       <Routes>
@@ -19,7 +46,6 @@ const App = () => {
         </Route>
 
         <Route path = 'view/:resumeId' element = {<Preview />}/>
-        <Route path = 'login' element = {<Login />}/>
 
 
       </Routes>
