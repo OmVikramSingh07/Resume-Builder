27d1ce943223bfaed60a0cb42b1d63a93a7dfaa0
vikram singh <omvikramsingh07@gmail.com>
2026-06-13 17:53:42 +0530

    run

--- Server/controllers/aiController.js
+++ Server/controllers/aiController.js
@@ -1,3 +1,5 @@
+import Resume from "../models/Resume.js";
+import ai from "../configs/ai.js";
 
 
 
@@ -47,7 +49,7 @@ export const enhanceJobDescription = async (req, res) => {
              model: process.env.OPENAI_MODEL,
                 messages: [
                     {   role: "system",
-                        content: "You are an expert in resume writing. Your task is to enhance the professional summary of a resume. The summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-friendly. And only return text no options or anything else." 
+                        content: "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be only 1-2 sentences also highlighting key responsibilities and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. And only return text no options or anything else." 
                     },
                     {
                         role: "user",
@@ -57,6 +59,92 @@ export const enhanceJobDescription = async (req, res) => {
         })
 
         const enhanceContent = response.choices[0].message.content;
+        return res.status(200).json({enhanceContent})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }    
+}
+
+
+
+export const uploadResume = async (req, res) => {
+    try {
+        
+        const {resumeText, title} = req.body;
+        const userId = req.userId;
+
+        if(!resumeText){
+            return res.status(400).json({message: 'Missing required fields'})
+        }
+
+        const systemPrompt = 'you are an expert AI Agent to extract data from resume.'
+
+        const UserPrompt = `extract data from this resume: ${resumeText} 
+        Provide data in the following JSON format with no additional text before or after:
+        
+        {professional_summary: { type: String, default: ''},
+    skills: [{ type: String }],
+    personal_info: {
+    image: {type: String, default: ''},
+    full_name: {type: String, default: ''},
+    profession: {type: String, default: ''},
+    email: {type: String, default: '' },
+    phone: {type: String, default: ''},
+    Location: {type: String, default: ''},
+    linkedin: {type: String, default: ''},
+    website: {type: String, default: ''},
+    },
+    experience: [
+        {
+            company: {type: String},
+            position: {type: String},
+            start_date: { type: String },
+            end_date: { type: String },
+            description: { type: String },
+            is_current: { type: Boolean },
+        }
+    ],
+
+    project: [
+        {
+            name: {type: String},
+            type: {type: String},
+            description: {type: String},
+        }
+    ],
+
+    education: [
+        {
+            institution: { type: String },
+            degree: { type: String },
+            field: { type: String },
+            graduation_date: { type: String },
+            gpa: {type: Boolean },   
+        }
+    ],
+    }
+
+        `
+
+        const response = await ai.chat.completion.create({
+             model: process.env.OPENAI_MODEL,
+                messages: [
+                    {   role: "system",
+                        content: systemPrompt 
+                    },
+                    {
+                        role: "user",
+                        content: UserPrompt,
+                    },
+                ],
+                response_format: {type: 'json_object'}
+        })
+
+        const extractedData = response.choices[0].message.content;
+        const parsedData = JSON.parse(extractedData)
+        const newResume = await Resume.create({userId, title, ...parsedData})
+
+        res.json({resumeId: newResume._id})
         return res.status(200).json({enhanceContent})
     } catch (error) {
         return res.status(400).json({message: error.message})
