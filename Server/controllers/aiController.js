d773f9b0c5d9aedec272ea030276471b7354f393
vikram singh <omvikramsingh07@gmail.com>
2026-06-13 16:59:05 +0530

    k

--- Server/controllers/aiController.js
+++ Server/controllers/aiController.js
@@ -27,3 +27,38 @@ export const enhanceProfessionalSummary = async (req, res) => {
         return res.status(400).json({message: error.message})
     }    
 }
+
+
+
+
+
+
+
+
+
+export const enhanceJobDescription = async (req, res) => {
+    try {
+        const {userContent} = req.body;
+
+        if(!userContent){
+            return res.status(400).json({message: 'Missing required fields'})
+        }
+        const response = await ai.chat.completion.create({
+             model: process.env.OPENAI_MODEL,
+                messages: [
+                    {   role: "system",
+                        content: "You are an expert in resume writing. Your task is to enhance the professional summary of a resume. The summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-friendly. And only return text no options or anything else." 
+                    },
+                    {
+                        role: "user",
+                        content: userContent,
+                    },
+                ],
+        })
+
+        const enhanceContent = response.choices[0].message.content;
+        return res.status(200).json({enhanceContent})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }    
+}
